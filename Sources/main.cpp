1457b825c664db6bf6a5034e3f520aa6c3dae128
Liyuchao <2038429915@qq.com>
2022-01-03 08:42:11 +0800

    Changes to be committed:

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+class Setting {
+public:
+	static void initialization() {
+		ifstream fin("config.txt");
+		if(!fin.is_open())
+			clog<<"[ERROR] Configuration read failed!\n";
+		for(string line;fin>>line;) {
+			int del=line.find('=');
+			cout<<line<<endl;
+			string var=line.substr(0,del);
+			if()
+		}
+	}
+};
 int main() {
-	cout<<"Minesweeping"<<endl;
+	Setting::initialization();
 	return 0;
 }

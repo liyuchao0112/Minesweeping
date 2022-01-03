05d219a564a642bbf5ef106ef5a1ec3db0c66be9
Liyuchao <2038429915@qq.com>
2022-01-03 19:04:57 +0800

    Changes to be committed:

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -4,13 +4,32 @@
 #include "../Headers/Map.hpp"
 using namespace std;
 Setting settings;
+Map map;
+void run() {
+	while(ture) {
+		map.print();
+		cout<<"1.标记地雷\n"
+			<<"2.标记安全\n"
+			<<"3.exit\n";
+		int t;
+		cin>>t;
+		switch(t) {
+		case 1:
+			int x,y;
+			cin>>x>>y;
+		case 2:
+			int x,y;
+			cin>>x>>y;
+		}
+	}
+}
 int main() {
 	cout<<"Loading...\n";
 	settings.initialization();
 	cout<<"Finish\n";
-	Map map(settings.mapHeight,settings.mapWeight,settings.landmineNumber);
+	map=Map(settings.mapHeight,settings.mapWeight,settings.landmineNumber);
 	cout<<"Game Start!\n";
-	map.print();
-	system("pause");
+	system("cls");
+	run();
 	return 0;
 }
